46a48cec36cdb56a7ea7363555c3a4fc6c87def6
Albert Chang <aznachang@gmail.com>
2017-03-29 23:45:04 -0700

    refactored HashTable
    refactored HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,31 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) { //O(1)
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var innerArray = [];
-  if (this._storage.get(index) === undefined) {
-    innerArray.push([k, v]);
-    this._storage.set(index, innerArray);
-  } else {
-    var result = this._storage.get(index);
-    result.push([k, v]);
-    this._storage.set(index, result);
+HashTable.prototype.insert = function(key, value){
+  // call hashing function - get a Bucket #
+  var index = getIndexBelowMaxForKey(key, storageLimit);
+  
+  // check to see if Bucket contains any items!
+  if (!this.storage[index]){
+    this.storage[index] = [[key, value]];
+  } 
+  	// bucket contains items
+  	else{
+    var inserted = false;
+
+    for(var i = 0; i < this.storage[index].length; i++){
+      // bucket finds an existing key
+      if (this.storage[index][i][0] === key) {
+        // update key's value with new value
+        storage[index][i][1] = value;
+        inserted = true;
+      }
+    }
+    // if no existing key found
+    if(inserted === false){
+    	// push whole [key, value] pair
+      storage[index].push([key,value]);
+    }
   }
 };
 
@@ -31,18 +46,18 @@ HashTable.prototype.retrieve = function(k) { //O(n)
   return result1;
 };
 
-HashTable.prototype.remove = function(k) { //O(n)
-  var index = getIndexBelowMaxForKey(k, this._limit); 
-  var result = this._storage.get(index);
-  for (var i = 0; i < result.length; i++) {
-    var innerArray = result[i];
-    if (innerArray[0] === k) {
-      innerArray.splice(i, 1);
+HashTable.prototype.remove = function(key){
+  var index = getIndexBelowMaxForKey(key, storageLimit);
+  if (storage[index].length === 1 && storage[index][0][0] === key) {
+      delete storage[index];
+  } else{
+      for (var i = 0; i < storage[index]; i++){
+        if(storage[index][i][0] ===  key){
+          delete storage[index][i];
+        }
+      }
     }
-  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
