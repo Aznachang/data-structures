6ed2c5dd10f3949622e189f278bb8511ba40fcfd
Albert Chang <aznachang@gmail.com>
2017-04-05 23:16:43 -0700

    LinkedList PseudoClassical
    changed to pseudoclassical for linkedlist.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,73 +1,93 @@
 var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
-
-  list.addToTail = function(value) {
-    var newTail = Node(value);
-
-    if (!list.head) {
-      list.head = newTail;
-    }
-    if (list.tail) {
-      list.tail.next = newTail;
-    }
-
-    // assign new TailEnd Node
-    list.tail = newTail;
-  };
-
-  list.removeHead = function() {
-    var currHead = list.head;
-
-    // check to see if a headNode exists
-    if (list.head === null) {
-      return null; // empty linked-list
-    }
-
-    list.head = list.head.next;
-    return currHead.value; // return value of previous headNode
-  };
-
-// recursive
-list.contains = function(target) {
-  var searchNode = function(obj) {
-    if ( obj.value === target) {
-      return true;
-    } else if (obj.next) {
-      return searchNode(obj.next)
-    } else {
-      return false;
-    }
-  }
-    return searchNode(list.head);
-  };
-
-  return list;
+	// THESE ARE JUST NODE POINTERS
+	// POINTERS TO NODES (value, 'next' pointer)
+	this.head = null; // contains node
+	// cur. Last LinkedList node
+	this.tail = null; // contains node
 };
 
 // iterative
-list.contains = function(target) {
+LinkedList.prototype.reverseLinkedList = function(head) {
+	let prev = null;
+	let curr = head;
+	let next;
 
-  var node = list.head;
-
-  while (node) {
-    if (node.value === target) {
-      return true;
-    }
-    node = node.next;
-  }
-    return false;
-  };
-
-  return list;
+	while(curr) {
+		// next - points to currNode's pointer
+		next = curr.next;
+		curr.next = prev;
+		// previousNode --> currNode
+		prev = curr;
+		// currNode - currNode's pointer 'next' Node
+		curr = next;
+	}
+	return prev;
 };
 
-var Node = function(value) {
-  var node = {};
+//recursive
+LinkedList.prototype.reverseLinkedList = function(head) {
+	// check to see if head.next exists 
+	if (!head.next) { return head; }
 
-  node.value = value;
-  node.next = null;
+	const nextNode = head.next;
+	const newHead = reverseLinkedList(nextNode);
 
-  return node;
+	head.next = null;
+	nextNode.next = head;
+
+	return newHead;
 };
+
+// cannot use 'this' - regular function
+LinkedList.prototype.makeNode = function(value) {
+	var newNode = {};
+	newNode.value = value;
+	newNode.next = null;	// points to newNode
+	return newNode;
+};
+
+LinkedList.prototype.addToTail = function(value) {
+	// pass-in value to newNode and have empty pointer
+	var newTail = this.makeNode(value);
+
+	// check if there exists even a Node
+	if (this.head === null) {
+		this.head = newTail;
+		// add to tailEnd of LinkedList if there is a headNode
+	} else {
+		// currTailNode's next (pointer to nextNode) should be null
+		this.tail.next = newTail;
+	}
+	// set newTail
+	this.tail = newTail;
+};
+
+LinkedList.prototype.removeHead = function() {
+	var currHeadNode = this.head;
+
+	// check to see if there is a headNode 
+	if (this.head === null) {
+		return null;
+		// if headNode and tailNode same --> only one Node in LinkedList!
+	} else if (this.head === this.tail) {
+		this.head = this.tail = null;
+	} else {
+		this.head = this.head.next;
+	}
+
+	return currHeadNode;
+};
+
+LinkedList.prototype.contains = function(target) {
+	// start at the headNode of the Chain!
+	var currNode = this.head;
+
+	// check to see if there is a Node currently to process
+	while (currNode) {
+		if (currNode.value === target) { return true; }
+
+		currNode = currNode.next;
+	} 
+
+	return false;
+};	
