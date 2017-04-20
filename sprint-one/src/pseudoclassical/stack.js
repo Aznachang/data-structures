90da730753bf7756005fdef55f14a76cf3cb1744
Albert Chang <aznachang@gmail.com>
2017-04-19 23:00:25 -0700

    added modular pop and getMin method

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -6,15 +6,30 @@ var Stack = function() {
 };
 
 Stack.prototype.push = function(value) {
-  this.storage[this.vSize] = value;
-  this.vSize++;
-
+  this.storage[this.vSize++] = value;
+  
   return value;
 };
 
-Stack.prototype.pop = function() {
-  var temp;
+Stack.prototype.pop = function(val) {
+	// check if there are any values in the stack
+	if (this._size === 0) {return null;}
 
+	for (var key in this._storage) {
+		if (this._storage[key] === val) {
+			delete this._storage[key];
+			this._size--;
+			return this._storage;
+			// return this.getMin();
+		}
+	} 
+
+	return this._storage;
+};
+
+Stack.prototype.popTop = function() {
+  var temp;
+  
   if (this.vSize > 0) {
     temp = this.storage[this.vSize - 1];
     delete this.storage[this.vSize - 1];
@@ -22,6 +37,18 @@ Stack.prototype.pop = function() {
 
     return temp;
   }
+};
+
+Stack.prototype.getMin = function() {
+	var min = this._storage[0];
+
+	for (var key in this._storage) {
+		if (this._storage[key] < min) {
+			min = this._storage[key];
+		}
+	}
+
+	return min;
 };
 
 Stack.prototype.size = function() {
